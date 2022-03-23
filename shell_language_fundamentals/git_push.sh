fe704c70005a67c819cdcf003f8dcc865b270768
Harman Kibue <34857700+harmannkibue@users.noreply.github.com>
2022-03-23 19:08:03 +0300

    Added the git push shell scripts

--- shell_language_fundamentals/git_push.sh
+++ shell_language_fundamentals/git_push.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+source ./functions/function.sh
+
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+
+echo "Pushing to branch ${BRANCH}"
+repeat_space 20
 echo -n "Enter commit message: "
 read COMMIT
+
 git add .
 git commit -m "$COMMIT"
-git push origin master
+git push origin ${BRANCH}
