625d41c08936bd63f612479f2bb541b0637d16b4
Harman Kibue <34857700+harmannkibue@users.noreply.github.com>
2022-03-26 10:49:35 +0300

    Added more options to git push shell

--- shell_language_fundamentals/git_push.sh
+++ shell_language_fundamentals/git_push.sh
@@ -7,9 +7,21 @@ BRANCH=`git rev-parse --abbrev-ref HEAD`
 echo "Pushing to branch ${BRANCH}"
 # Function call from the source import above
 repeat_space 2
+echo -n "Enter file path() to commit, (Press enter to commit all): "
+
+read FILE_PATH
+
+if [ -z "${FILE_PATH}" ]
+then
+#      echo "Path is empty is empty"
+      git add .
+else
+#      echo "Path is NOT empty ${FILE_PATH}"
+      git add $FILE_PATH
+fi
+
 echo -n "Enter commit message: "
 read COMMIT
 
-git add .
 git commit -m "$COMMIT"
-git push origin ${BRANCH}
+#git push origin ${BRANCH}
