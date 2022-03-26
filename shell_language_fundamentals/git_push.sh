0fd01abb6cebdc94225aa56de15bc470960f8eb0
Harman Kibue <34857700+harmannkibue@users.noreply.github.com>
2022-03-26 10:53:26 +0300

    Added git push changes

--- shell_language_fundamentals/git_push.sh
+++ shell_language_fundamentals/git_push.sh
@@ -6,17 +6,17 @@ BRANCH=`git rev-parse --abbrev-ref HEAD`
 
 echo "Pushing to branch ${BRANCH}"
 # Function call from the source import above
-repeat_space 2
+repeat_space 20
 echo -n "Enter file path() to commit, (Press enter to commit all): "
-
 read FILE_PATH
+
+repeat_space 20
+
 
 if [ -z "${FILE_PATH}" ]
 then
-#      echo "Path is empty is empty"
       git add .
 else
-      echo "Path is NOT empty ${FILE_PATH}"
       git add $FILE_PATH
 fi
 
